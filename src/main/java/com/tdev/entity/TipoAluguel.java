c30080cfd92e097178478504653e9d653034c1ed
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-06-21 18:01:52 -0300

    Tipo aluguel

---
+++ src/main/java/com/tdev/entity/TipoAluguel.java
@@ -0,0 +1,6 @@
+package com.tdev.entity;
+
+public enum TipoAluguel {
+
+	COMUN, EXTENDIDO, SEMANAL;
+}
---
+++ src/test/java/com/tdev/runners/RunnerTest.java
@@ -0,0 +1,19 @@
+package com.tdev.runners;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features/alugar_filme.feature",
+		glue = "com.tdev.steps",
+		tags = {},
+		plugin = {"pretty", "html:target/report-html"},
+		monochrome = true,
+		snippets = SnippetType.CAMELCASE,
+		strict = true)
+public class RunnerTest {
+
+}
